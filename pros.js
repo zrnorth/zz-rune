643313d402b77c01c641ceddfcf58718b1643193
Zach North <zacnorth@imdb.com>
2015-05-03 13:37:34 -0700

    added pro id numbers

--- pros.js
+++ pros.js
@@ -1,11 +1,11 @@
 [
-    "Turtle the Cat",
-    "Pobelter",
-    "Eugene J Park",
-    "Doublelift",
-    "C9 Sneaky",
-    "I am Bjerg",
-    "Popobelterold",
-    "HotGuy6Pack",
-    "TiP Apollo"
+    "18991200", /* Turtle the Cat */
+    "2648", /* Pobelter */
+    "51409228", /*Eugene J Park */
+    "20132258", /* Doublelift */
+    "51405", /* C9 Sneaky */
+    "63581619",/* I am Bjerg */
+    "51539240", /* Popobelterold */
+    "57029179", /* HotGuy6Pack */
+    "7250" /* TiP Apollo */
 ]
