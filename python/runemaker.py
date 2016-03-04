89a9aeebc403c228acbfbd13a105fa31216dcc98
Zach North <zrnorth@gmail.com>
2016-03-04 00:11:35 -0800

    Added the get_runepages functionality, which takes in champ names and # of runepages and gets the most common pages

--- python/runemaker.py
+++ python/runemaker.py
@@ -4,6 +4,7 @@ Contains the logic to make a rune set based on player input.
 
 import championgg_api
 import util
+import pprint
 
 def get_runes_for_champs(champions):
     """
@@ -13,6 +14,9 @@ def get_runes_for_champs(champions):
     for champion in champions:
         roleRuneMap = championgg_api.get_most_winning_runes(champion)
         for runeset in roleRuneMap:
+            if runeset == 'error': # got a bad runeset, probably bad champ name input.
+                continue
+                
             role = runeset['role']
             runeGroups = runeset['runes']
             runeShorthands = []
@@ -35,12 +39,37 @@ def merge_all_common_runesets(champRoleRunesList):
         key = ", ".join(runesShorthand)
         
         if key in dict: # Collision! combine the values.
-            newKey = dict[key][0]
-            newKey.append(champRole)
-            newScore = dict[key][1] + 1
-            dict[key] = (newKey, newScore)
+            newScore = dict[key][0] + 1
+            champRoleList = dict[key][1]
+            champRoleList.append(champRole)
+
+            dict[key] = (newScore, champRoleList)
         else: # New key, so add to the dict
-            dict[key] = ([champRole], 1)
+            dict[key] = (1, [champRole])
     
     return dict
+    
+def get_runepages(championList, maxPages):
+    """
+    Given a list of champions and a maximum number of pages, returns a mapping of
+    runepages to champions they are used for. Also returns a list of "left-out" champs.
+    """
+    runeSets = get_runes_for_champs(championList)
+    merged = merge_all_common_runesets(runeSets)
+    
+    results = []
+    leftOut = []
+    count = 0
+    # Sort by the "score" of a runepage, to remove the ones that are too low in priority (if necessary)
+    for key in sorted(merged, key=merged.get, reverse=True):
+        if count < maxPages:
+            results.append( (key, merged[key]) )
+            count += 1
+        else:
+            leftOut.append( (key, merged[key]) )
+    print("Results: ")
+    print(results)
+    if leftOut:
+        print("These pages were left out: ")
+        print(leftOut)
     
