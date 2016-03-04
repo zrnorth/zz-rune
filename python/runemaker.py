da03ebae07526c8fd2991c4765dcf4b379563cb8
Zach North <zrnorth@gmail.com>
2016-03-03 23:35:24 -0800

    Added new function to get all common runesets

--- python/runemaker.py
+++ python/runemaker.py
@@ -6,6 +6,9 @@ import championgg_api
 import util
 
 def get_runes_for_champs(champions):
+    """
+    Given a list of champions, returns a list of tuples of the form [(ChampRole, [RunesShorthand])]
+    """
     results = []
     for champion in champions:
         roleRuneMap = championgg_api.get_most_winning_runes(champion)
@@ -21,3 +24,23 @@ def get_runes_for_champs(champions):
             results.append( (championRoleString, runeShorthands) )
     
     return results
+    
+def merge_all_common_runesets(champRoleRunesList):
+    """
+    Given a list of form [(ChampRole, [RunesShorthand])], consolidates the common sets
+    and returns a list of for [( ( [ChampRole1, ChampRole2, ...], [RunesShorthand] )]
+    """
+    dict = {}
+    for champRole, runesShorthand in champRoleRunesList:
+        key = ", ".join(runesShorthand)
+        
+        if key in dict: # Collision! combine the values.
+            newKey = dict[key][0]
+            newKey.append(champRole)
+            newScore = dict[key][1] + 1
+            dict[key] = (newKey, newScore)
+        else: # New key, so add to the dict
+            dict[key] = ([champRole], 1)
+    
+    return dict
+    
